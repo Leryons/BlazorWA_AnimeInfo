5394ca3d04caae04e5a68c112231b8bcc2f4c4bf
Leryons <pulgarleonardo22@gmail.com>
2025-04-07 14:58:28 -0400

    Update

--- GlobalUsing.cs
+++ GlobalUsing.cs
@@ -1,8 +1,9 @@
 ﻿global using AnimeInfo.Data.Services;
 global using AnimeInfo.Data.Models;
-global using AnimeInfo.Pages;
 
 //Tool
 global using Newtonsoft.Json.Linq;
 global using Microsoft.AspNetCore.Components;
 global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+global using AnimeInfo;
--- Program.cs
+++ Program.cs
@@ -1,7 +1,3 @@
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using AnimeInfo;
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
